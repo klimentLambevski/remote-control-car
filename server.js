84876223a01cc38dddbcb19954e1fdd10f940372
kliment <kliment@vitech.dev>
2025-04-14 00:02:55 +0200

    Implement car driving simulation

---
+++ server.js
@@ -0,0 +1,38 @@
+// server.js
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+// Use the public folder for static files (HTML, CSS, client-side JS)
+app.use(express.static(__dirname + '/public'));
+
+// Define routes for the HTML pages
+app.get('/controller', (req, res) => {
+    res.sendFile(__dirname + '/public/car-control.html');
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/car-game.html');
+});
+
+// Socket.IO connection logic
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    // Listen for chat messages from the client
+    socket.on('COMMAND', (msg) => {
+        console.log('Message received: ' + msg);
+        // Broadcast the message to all connected clients
+        io.emit('COMMAND', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+// Start the server on port 3000
+http.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
